cce803ddf3c3fe9f6808352b356570a418caaa98
shahedmhsnna239 <sh0567967969@gmail.com>
2023-08-02 12:22:58 +0300

    fifth commit

---
+++ venv/task2.py
@@ -0,0 +1,10 @@
+username = input("FullName")
+print(username)
+year_of_birth = input("Year_of_Birth")
+year_of_birth = int(year_of_birth)
+print(year_of_birth)
+current_year = 2023
+user_age = current_year - year_of_birth
+print(user_age)
+mobile_number = input("mobile_number like05x-xxx-xxxx")
+print(mobile_number.split("-"))
