63ec6ccee1b93cbe37309683d1f61caa40723075
shahedmhsnna239 <sh0567967969@gmail.com>
2023-08-02 11:57:31 +0300

    third commit

--- task1.py
+++ task1.py
@@ -1,6 +1,3 @@
-name = input("enter your name")
-print(name)
-age = input("enter your age")
-print(age)
-birthday = input("enter your birthday")
-print(birthday)
+
+a = "Hallow,World"
+print(a.split(","))
