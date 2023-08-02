5d6591ce4b6537ff2afa7c08602f9e05a4bc1c68
shahedmhsnna239 <sh0567967969@gmail.com>
2023-08-02 23:31:35 +0300

    six commit

---
+++ task3.py
@@ -0,0 +1,19 @@
+num1 = int(input("Enter num1"))
+num2 = int(input("Enter num2"))
+
+def add(num_1,num_2):
+    return num_1 + num_2
+
+def subtraction(num_1,num_2):
+    return num_1 - num_2
+
+def multiplication(num_1,num_2):
+    return num_1 * num_2
+
+def division(num_1,num_2):
+    return num_1 / num_2
+
+print(add(num1,num2))
+print(subtraction(num1,num2))
+print(multiplication(num1,num2))
+print(division(num1,num2))
