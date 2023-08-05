6bc7645d3cb32991b01f77da1a788ff14be0bbeb
shahedmhsnna239 <sh0567967969@gmail.com>
2023-08-05 11:15:02 +0300

    seven commit

--- task1.1.py
+++ task1.1.py
@@ -1,16 +1,46 @@
 def triangle():
-    x = int(input("enter a base"))
-    y = int(input("enter a rib 1"))
-    z = int(input("enter a rib 2"))
-    a = int(input("enter a rise"))
-    area = 0.5 * x * a
-    per = x + y + z
+    base = int(input("enter a base"))
+    rid1 = int(input("enter a rib 1"))
+    rid2 = int(input("enter a rib 2"))
+    rise = int(input("enter a rise"))
+    area = 0.5 * base * rise
+    per = base + rid1 + rid2
     print(area , per)
+
+    if area >= 10:
+        print("large")
+    elif area < 10:
+        print("small")
+    elif area <=0:
+        print("invaildinput")
 triangle()
 
+
 def square():
-    x = int(input("enter a rid"))
-    area = x * x
-    per = x + x + x + x
+    rid = int(input("enter a rid"))
+    area = rid * rid
+    per = rid + rid + rid + rid
     print(area , per)
+    if area >= 10:
+        print("large")
+    elif area < 10:
+        print("small")
+    elif area <= 0:
+        print("invaildinput")
+
 square()
+
+def circle():
+    radius = int(input("enter a radius"))
+    area = radius * radius * 3.14
+    per = 2 * radius * 3.14
+    print(area , per)
+    if area >= 10:
+        print("large")
+    elif area < 10:
+        print("small")
+    elif area <= 0:
+        print("invaildinput")
+
+
+circle()
