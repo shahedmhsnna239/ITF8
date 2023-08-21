28d3f73021916cab95cdcf7926680d1bc0372e86
shahedmhsnna239 <sh0567967969@gmail.com>
2023-08-21 16:18:41 +0300

    eleven commit

--- dict.py
+++ dict.py
@@ -1,49 +1,45 @@
+
 products = []
 while True:
-    selection = int(input("1.Add New Product\n2.Print Product Details"
-                          "\n3.Buy Product"
-                          "\n4.Delete Product"
-                          "\n5.Exit"))
+    selection = int(input("1.add product\n2.print details\n"
+                          "3.buy product\n"
+                          "4.delete product\n"
+                          "5.exit"))
     if selection == 1:
-        product_number = input("Enter Product Number :")
-        product_name = input("Enter Product Name :")
-        product_price = input("Enter Product Price :")
-        product_qty =  int(input("Enter Product Quantity :"))
+        product_num = int(input("enter the product number"))
+        product_name = input("enter the product name")
+        product_price = int(input('enter product price'))
+        product_qty = int(input("enter product quantity"))
         product = {
-            "id" : product_number,
+            "id" : product_num,
             "name" : product_name,
             "price" : product_price,
             "qty" : product_qty
         }
         products.append(product)
-        print("Product Added Successfully")
+        print("product added successfully")
     elif selection == 2:
-        product_number = input("Enter product number :")
+
         for i in products:
-            if i['id'] == product_number:
-                print(i)
-                break
+            print(i)
+            break
     elif selection == 3:
-        print("3")
-        num = int(input("product_number"))
-        for i in products :
-            if i["id"] == product_number :
-                while True :
-                    qty = int(input("Enter product qty :"))
-                    if qty > i["qty"] or qty <= 0 :
-                        print("Invalid Qty")
+        product_num = int(input('enter the product number'))
+        for i in products:
+            if i['id'] == product_num:
+                while True:
+                    qty = int(input("enter product quantity"))
+                    if qty > i['id'] or qty <= 0:
+                        print("invalid quantity")
                     else:
                         break
-                i["qty"] = i["qty"] _ qty
+                i['qty'] = i['qty'] - qty
                 break
-
     elif selection == 4:
-        product_number = input("Enter product number :")
+        product_number = input('enter product number')
         for i in products[:]:
-            if i["id"] == product_number :
+            if i['id'] == product_number:
                 products.remove(i)
                 break
-
-        print(4)
     else:
         exit()
